3eea3493f3b31c21dc9f1faeb37a65bdbfdb8eeb
PaulaMorillasAlonso <pamorill@ucm.es>
2024-05-08 11:32:00 +0200

    eventos se guardan en carpeta

--- Events/eventSender.py
+++ Events/eventSender.py
@@ -1,10 +1,14 @@
 import time
+import os
 class EventSender:
     def __init__(self, serializer, interval):
         self.serializer = serializer
         self.interval = interval
         self.events = []
-
+        self.tracked_events_folder = "TrackedEvents"
+        if not os.path.exists(self.tracked_events_folder):
+            os.makedirs(self.tracked_events_folder)
+        
     def add_event(self, event):
         self.events.append(event)
 
@@ -15,7 +19,7 @@ class EventSender:
 
     def save_events(self):
         
-        filename = f"event_{int(time.time())}{self.serializer.get_file_extension()}"
+        filename = f"{self.tracked_events_folder}/event_{int(time.time())}{self.serializer.get_file_extension()}"
         with open(filename, 'w') as file:
             file.write(self.serializer.init_file_format())
             for event in self.events:
