24ab9df1ec6e0d24876d67fd71bec23e9c65cfe0
Laura <lgomez25@ucm.es>
2024-05-09 17:28:09 +0200

    Update main.py

--- DataAnalyzer/main.py
+++ DataAnalyzer/main.py
@@ -10,34 +10,22 @@ class dataAnalyzer:
     with open('prueba.json', 'r') as json_data:
         data = json.load(json_data)
     eyePosData = data["eye_positions"]
-    # while True:
-
-    #     firstDataTime = input("First data time: ")
-    #     lastDataTime = input ("Last data time: ")
-    #     try:
-    #         firstDataTime= int(firstDataTime)
-    #         lastDataTime= int(lastDataTime)
-    #         if(firstDataTime <= lastDataTime):
-    #             break
-    #         else:
-    #             print("The second number must be larger than the first one")
-    #     except ValueError:
-    #         print("Invalid data types")
+    
     initTime = eyePosData[0]['timestamp']
 
-    startDataTime = 3
-    endDataTime= 10
+    startDataTime = 0
+    endDataTime= len(eyePosData)
 
     #Avanzar hasta la primera marca de tiempo que se necesita
     for j in range(0, len(eyePosData)):
-        if(eyePosData[j]['timestamp'] >= (initTime + 3)):
+        if(eyePosData[j]['timestamp'] >= (initTime + startDataTime)):
             break
 
     print(j)
     print(eyePosData[j]['timestamp'])
     totalPositions= []
     for k in range(j,len(eyePosData)):
-        if(eyePosData[k]['timestamp'] > (initTime + 100)):
+        if(eyePosData[k]['timestamp'] > (initTime + endDataTime)):
             break
         print
         totalPositions.append((eyePosData[k]['posX'], eyePosData[k]['posY']))
