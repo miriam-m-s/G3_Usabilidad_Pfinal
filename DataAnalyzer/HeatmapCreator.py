2952d3fd81574e8d1b0aa9bba0c68f9f6b2bf660
Laura Gomez <lgomez25@ucm.es>
2024-05-16 20:04:21 +0200

    Análisis con los datos obtenidos hasta ahora

--- DataAnalyzer/HeatmapCreator.py
+++ DataAnalyzer/HeatmapCreator.py
@@ -17,11 +17,19 @@ class dataAnalyzer:
         for c in coords:
             xCoord= math.trunc(c[0] * 20)
             yCoord= math.trunc(c[1] * 10)
+            if(xCoord < 0):
+                xCoord = 0
+            if(xCoord > 20):
+                xCoord = 20
+            if(yCoord<0):
+                yCoord =0
+            if(yCoord>10):
+                yCoord=10
             heatData.iat[yCoord,xCoord] +=1 
         #Creación del heatmap
         plt.figure(figsize=(10,10))
-        map = sns.heatmap(heatData,cmap='coolwarm', annot=True,  square=True, linewidths=.5)
-        map.set_title('Mapa de calor según tiempo', fontsize =16)
+        map = sns.heatmap(heatData,cmap='coolwarm', annot=True,  square=True, linewidths=.5, cbar=False)
+        map.set_title('Mapa de calor según tiempo que se ha mirado a cada punto', fontsize =16)
         plt.show()
     #Crea un mapa de calor según cuantas veces se ha mirado cada posición de la pantalla
     def createCountHeatMap(coords):
@@ -35,6 +43,14 @@ class dataAnalyzer:
         for c in coords:
             xCoord= math.trunc(c[0] * 20)
             yCoord= math.trunc(c[1] * 10)
+            if(xCoord < 0):
+                xCoord = 0
+            if(xCoord > 20):
+                xCoord = 20
+            if(yCoord<0):
+                yCoord =0
+            if(yCoord>10):
+                yCoord=10
             #Comprueba que la posición no sea igual a la anterior para añadirla al mapa
             if(prevXcoord == xCoord and prevYcoord == yCoord):
                 continue
@@ -43,38 +59,32 @@ class dataAnalyzer:
             prevYcoord = yCoord
         #Creación del heatmap
         plt.figure(figsize=(10,20))
-        map = sns.heatmap(heatData,cmap='viridis', annot=True,  square=True, linewidths=.5)
+        map = sns.heatmap(heatData,cmap='viridis', annot=True,  square=True, linewidths=.5, cbar=False)
         map.set_title('Mapa de calor según cantidad de veces que se ha mirado un punto', fontsize=12)
         plt.show()
-    def readData(nameFile):
+        
+    def readData(nameFile,init, duration):
         # Cargar el JSON desde un archivo
         with open(nameFile, 'r') as json_data:
             data = json.load(json_data)
         eyePosData = data["Events"]
-        print (eyePosData[0])
-        initTime = math.trunc(eyePosData[0]['timestamp'])
-        print(initTime)
-        startDataTime = 0
-        endDataTime= len(eyePosData)
-
+        initTime = math.trunc(eyePosData[1]['timestamp'])
+        
+        startDataTime = init
+        durationData= duration
         #Avanzar hasta la primera marca de tiempo que se necesita
-        for j in range(0, len(eyePosData)-1):
+        for j in range(1, len(eyePosData)-1):
             if(math.trunc(eyePosData[j]['timestamp']) >= (initTime + startDataTime)):
                 break
-        posicionespochas=0
         totalPositions= []
         for k in range(j,len(eyePosData)-1):
-            print(k)
-            if(math.trunc(eyePosData[k]['timestamp']) > (initTime + endDataTime)):
+            if(math.trunc(eyePosData[k]['timestamp']) > (initTime + startDataTime+durationData)+1):
                 break
             # No meter las posiciones de x e y que se salgan de la pantalla
-            if(eyePosData[k]['posX'] > 1 or eyePosData[k]['posX'] < 0 or eyePosData[k]['posY'] > 1 or eyePosData[k]['posY'] < 0):
-                posicionespochas+=1
-                continue
+            #if(eyePosData[k]['posX'] > 1 or eyePosData[k]['posX'] < 0 or eyePosData[k]['posY'] > 1 or eyePosData[k]['posY'] < 0):
+                #continue
             totalPositions.append((eyePosData[k]['posX'], eyePosData[k]['posY']))
-            print(posicionespochas)
-        createTimeHeatMap(totalPositions)
-        createCountHeatMap(totalPositions)
+        return totalPositions
 
 
 
