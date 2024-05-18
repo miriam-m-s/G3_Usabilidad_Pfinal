cd8de408f0c65bede3a7fc7b9c3a92e6593d30fb
Miguel González Pérez <miggon23@ucm.es>
2024-05-18 16:28:28 +0200

    Bug corregido de nuevo

--- App/Tabs/resultsTab.py
+++ App/Tabs/resultsTab.py
@@ -54,8 +54,8 @@ class ResultsTab(Tab):
        
     def load_user_test_dirs(self):
         listdir = []    
+        user_tests_dir = Consts.USER_TESTS_DIR
         if os.path.exists(user_tests_dir) and os.path.isdir(user_tests_dir):
-            user_tests_dir = Consts.USER_TESTS_DIR
             for item in os.listdir(user_tests_dir):
                 item_path = os.path.join(user_tests_dir, item)
                 if os.path.isdir(item_path):
